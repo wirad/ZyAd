fc7aa8bd915367a3d1f572e506f6469486bdf74a
Hao <wirad@163.com>
2025-11-27 17:15:37 +0800

    init

---
+++ library/src/main/java/com/adunity/library/AdUtils.java
@@ -0,0 +1,16 @@
+package com.adunity.library;
+
+/*
+ *  项目名:   Ad
+ *  包名:     com.adunity.library
+ *  文件名:   AdUtils
+ *  创建时间:  2025/11/27
+ *  创建者:   Hao
+ *  描述:
+ */
+public class AdUtils {
+
+    public static String getVersion() {
+        return "1.0.0";
+    }
+}
